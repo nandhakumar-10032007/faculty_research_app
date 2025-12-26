2540800632fdfb98d0bb7a464277366be109d502
Nantha <nandhakumarr.cse2024@citchennai.net>
2025-12-26 20:50:46 +0530

    Resolve main.dart merge conflict and set admin login as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // IMPORTANT
 import 'pages/admin_login_page.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
